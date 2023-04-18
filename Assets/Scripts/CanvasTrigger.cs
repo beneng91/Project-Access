47e2f9abf9961a22fec4d933f3f5e397ae8ba7a8
beneng91 <103171571+beneng91@users.noreply.github.com>
2023-04-18 16:59:58 +1000

    stuff

--- Assets/Scripts/CanvasTrigger.cs
+++ Assets/Scripts/CanvasTrigger.cs
@@ -14,6 +14,8 @@ public class CanvasTrigger : MonoBehaviour
     private int layerKey;
     private int layerInteractable;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
